0ee04be0bc638fa20173d236be774afa265d15ce
BryanBarre <barrebryan289@gmail.com>
2021-04-17 13:57:22 +0200

    avant réarangement

--- app/src/main/java/com/example/campusmap/MapActivity.java
+++ app/src/main/java/com/example/campusmap/MapActivity.java
@@ -1,32 +1,16 @@
 package com.example.campusmap;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.Constraints;
 
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.renderscript.Sampler;
-import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.example.campusmap.Graph.Vertex;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
-import java.io.EOFException;
-import java.io.IOException;
-import java.util.jar.Attributes;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
     MapView map;
@@ -34,6 +18,10 @@ public class MapActivity extends AppCompatActivity {
     float height;
     float width;
 
+    //------------------------------------------------------------------------------
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    //------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +37,16 @@ public class MapActivity extends AppCompatActivity {
         Spinner etage = (Spinner) findViewById(R.id.etage);
         Spinner salle = (Spinner) findViewById(R.id.salle);
 
+        map = (MapView) findViewById(R.id.map);
+
+        //------------------------------------------------------------------------------
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        //------------------------------------------------------------------------------
 
         final TextView resultat = (TextView) findViewById(R.id.resultat);
         Button astar = (Button) findViewById(R.id.astar);
 
         map = (MapView) findViewById(R.id.map);
-
 
         switch (QrCode) {
             case "Rdc_Code1"://positioner les case comme cela equivaut a if(x || y)
@@ -75,8 +67,6 @@ public class MapActivity extends AppCompatActivity {
             case "Etage2_Code3":
                 map.setBackgroundResource(R.drawable.etage2);
                 break;
-
-
         }
 
         astar.setOnClickListener(new View.OnClickListener() {
@@ -137,7 +127,7 @@ public class MapActivity extends AppCompatActivity {
         super.onStop();
     }
 
-    public void initializePoint(String etage){
+    public void initializePoint(String etage) {
         Resources res = getResources();
         map.graph.clear();
         String[] pointX;
@@ -148,50 +138,64 @@ public class MapActivity extends AppCompatActivity {
             pointX = res.getStringArray(R.array.RDCpointsX);
             pointY = res.getStringArray(R.array.RDCpointsY);
             pointName = res.getStringArray(R.array.RDCname);
-            link=res.getStringArray(R.array.RDClink);
-        }
-        else if (etage.equals("ET1")){
+            link = res.getStringArray(R.array.RDClink);
+        } else if (etage.equals("ET1")) {
             pointX = res.getStringArray(R.array.ET1pointsX);
             pointY = res.getStringArray(R.array.ET1pointsY);
             pointName = res.getStringArray(R.array.ET1name);
-            link=res.getStringArray(R.array.ET1link);
+            link = res.getStringArray(R.array.ET1link);
 
-        }
-        else{
+        } else {
             pointX = res.getStringArray(R.array.ET2pointsX);
             pointY = res.getStringArray(R.array.ET2pointsY);
             pointName = res.getStringArray(R.array.ET2name);
-            link=res.getStringArray(R.array.ET2link);
+            link = res.getStringArray(R.array.ET2link);
         }
 
         float x;
         float y;
         String name;
-        int cpt=0;
-        height=map.getHeight();
-        width=map.getWidth();
+        int cpt = 0;
+        height = map.getHeight();
+        width = map.getWidth();
         try {
-            while (true){
+            while (true) {
                 x = Float.parseFloat(pointX[cpt]) * (width / 864);
                 y = Float.parseFloat(pointY[cpt]) * (height / 864);
                 name = pointName[cpt];
                 map.graph.addVertex(name, x, y);
                 cpt++;
             }
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("fin de fichier: "+e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("fin de fichier: " + e);
         }
 
         try {
-            cpt=0;
-            while (true){
-                map.graph.addEdge(link[cpt],link[cpt+1]);
-                cpt=cpt+2;
+            cpt = 0;
+            while (true) {
+                map.graph.addEdge(link[cpt], link[cpt + 1]);
+                cpt = cpt + 2;
             }
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("fin de fichier: "+e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("fin de fichier: " + e);
         }
 
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            map.setScaleX(mScaleFactor);
+            map.setScaleY(mScaleFactor);
+            return true;
+        }
+    }
+    
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
     }
 
 }
